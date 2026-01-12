66b28a5dfa737a22b07e2d326ccf58fe36ef2ad1
candylink69 <omomshanti079@gmail.com>
2026-01-12 18:15:31 +0530

    Update data.js

--- data.js
+++ data.js
@@ -3,31 +3,32 @@ const videos = [
     id: "V001",
     title: "",
     duration: "43:53"
-    categories: [professional]
+    categories: ["professional"]
   },
    {
     id: "V002",
     title: "",
     duration: "50:04"
-    categories: [professional]
+    categories: ["professional"]
   },
   {
     id: "V003",
     title: "",
     duration: "45:58"
-    categories: [professional]
+    categories: ["professional"]
   },
   {
     id: "V004",
     title: "",
     duration: "1:01:34"
-    categories: [professional]
+    categories: ["professional"]
   },
   
   {
     id: "I001",
     title: "",
     duration: "19:34"
-    categories: [Indian]
+    categories: ["Indian"]
   }
 ]
+
