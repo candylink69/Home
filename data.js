fefbc78d158bdfc8982d00fa949723f64b0d5e2b
216120306076 <omomshanti079@gmail.com>
2026-02-03 10:15:22 +0530

    Update data.js

--- data.js
+++ data.js
@@ -3,53 +3,42 @@ const videos = [
     id: "V001",
     title: "",
     duration: "43:53",
-    thumbnail": "Thumbs/V001.webp",
     categories: ["professional"]
   },
   {
     id: "V002",
     title: "",
     duration: "50:04",
-    thumbnail": "Thumbs/V002.webp",
     categories: ["professional"]
   },
   {
     id: "V003",
     title: "",
     duration: "45:58",
-    thumbnail": "Thumbs/V003.webp",
     categories: ["professional"]
   },
   {
     id: "V004",
     title: "",
     duration: "1:01:34",
-    thumbnail": "Thumbs/V004.webp",
     categories: ["professional"]
   },
   {
     id: "I001",
     title: "",
     duration: "19:34",
-    thumbnail": "Thumbs/l001.webp",
     categories: ["indian"]
   },
   {
     id: "M001",
     title: "",
     duration: "29:46",
-    thumbnail": "Thumbs/M001.webp",
     categories: ["mom"]
   },
   {
     id: "S001",
     title: "",
     duration: "42:04",
-    thumbnail": "Thumbs/S001.webp",
     categories: ["sister"]
   }
 ];
-
-
-
-
