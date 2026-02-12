f0c99400161080b659cbcddb13e5622acae0ae7c
216120306076 <omomshanti079@gmail.com>
2026-02-12 23:27:53 +0530

    Update data.js

--- data.js
+++ data.js
@@ -17,43 +17,51 @@ const videos = [
     id: "V003",
     title: "",
     duration: "45:58",
-    categories: ["professional"]
+    categories: ["professional"],
+    preview:
   },
   {
     id: "V004",
     title: "",
     duration: "1:01:34",
-    categories: ["professional"]
+    categories: ["professional"],
+    preview:
   },
   {
     id: "I001",
     title: "",
     duration: "19:34",
-    categories: ["indian"]
+    categories: ["indian"],
+    preview:
   },
   {
     id: "M001",
     title: "",
     duration: "29:46",
-    categories: ["mom"]
+    categories: ["mom"],
+    preview:
   },
   {
     id: "S001",
     title: "Stepbrother thinks his sister is a lesbian but she proves him wrong",
     duration: "42:04",
-    categories: ["sister"]
+    categories: ["sister"],
+    preview:
   },
   {
     id: "I002",
     title: "",
     duration: "11:22",
-    categories: ["indian"]
+    categories: ["indian"],
+    preview:
   },
   {
     id: "S002",
     title: "",
     duration: "33:41",
-    categories: ["sister"]
+    categories: ["sister"],
+    preview:
   }
 ];
+
 
