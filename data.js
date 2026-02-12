f7188bfa01740ecff0f0a2172d81babe5fa41f83
216120306076 <omomshanti079@gmail.com>
2026-02-12 23:32:44 +0530

    Update data.js

--- data.js
+++ data.js
@@ -18,50 +18,51 @@ const videos = [
     title: "",
     duration: "45:58",
     categories: ["professional"],
-    preview:
+    preview: "https://candylink69.embed4me.vip/wgUK0l8R4QoSiKwlSR6RWw/xq/j3tyqzox/pqdcic/preview.webp"
   },
   {
     id: "V004",
     title: "",
     duration: "1:01:34",
     categories: ["professional"],
-    preview:
+    preview: "https://candylink69.embed4me.vip/rEsM1ZLl-0W2X0YA_KvOtQ/miy/j3tyqzox/bled5e/preview.webp"
   },
   {
     id: "I001",
     title: "",
     duration: "19:34",
     categories: ["indian"],
-    preview:
+    preview: "https://candylink69.embed4me.vip/l32_EfA9UJPteokTGwc74w/q3o/j3tyqzox/id9j9y/preview.webp"
   },
   {
     id: "M001",
     title: "",
     duration: "29:46",
     categories: ["mom"],
-    preview:
+    preview: "https://candylink69.embed4me.vip/ZlAnuh1s0rhAIvE4_yUypw/miy/afyhwq5f/l8a8la/preview.webp"
   },
   {
     id: "S001",
     title: "Stepbrother thinks his sister is a lesbian but she proves him wrong",
     duration: "42:04",
     categories: ["sister"],
-    preview:
+    preview: "https://candylink69.embed4me.vip/yVrianvT4NxThVw044mVEg/mf/fmg9y9ce/nwg6r1/preview.webp"
   },
   {
     id: "I002",
     title: "",
     duration: "11:22",
     categories: ["indian"],
-    preview:
+    preview: "https://candylink69.embed4me.vip/Fkwieh6e54jU9lP7MmuRYQ/luy/fmg9y9ce/q1ihd1/preview.webp"
   },
   {
     id: "S002",
     title: "",
     duration: "33:41",
     categories: ["sister"],
-    preview:
+    preview: "https://candylink69.embed4me.vip/gQtkVVt9k94c9jcdSjsCuQ/m9/fmg9y9ce/3bslyx/preview.webp"
   }
 ];
+
 
 
